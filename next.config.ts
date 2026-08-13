0352fe0975f6593a6f1778a753257c83625daa03
Rafael <rafael.machado0995@gmail.com>
2026-08-13 18:14:30 -0300

    Fix next.config typescript exports

--- next.config.ts
+++ next.config.ts
@@ -1,2 +1,8 @@
+﻿import type { NextConfig } from 'next';
 
-export default { eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true } };
+const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
+
+export default nextConfig;
