6450c2391585a80c6ee5eff39321b21f66cff8b2
Rafael <rafael.machado0995@gmail.com>
2026-08-13 18:25:52 -0300

    Force bypass typescript with next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-﻿import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
-
 export default nextConfig;
